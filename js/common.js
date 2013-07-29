27ae83bf7c58859b641ec308d16c4070006d1297
akella <akella.a@gmail.com>
2013-07-29 16:53:26 +0300

    up sidebar

--- js/common.js
+++ js/common.js
@@ -15,4 +15,64 @@ $(document).ready(function() {
 	$('.sites').click(function (e) {
 		$(this).addClass('is-expanded');
 	});
+
+
+// lets have fun with scroll, oh dear
+	//elements
+	readwrap = $('.read__wrap');
+	header = $('.topline');
+	footer = $('.footer');
+	promoted  = $('.promoted ');
+	body = $('body');
+	//alert($('.footer').offset().top);
+
+	// static parameters
+	fh = footer.height();
+	fTop = footer.offset().top;
+	hh = 60;
+	ph  = promoted.height();
+	bh = $(document).height();
+	margintop = 70;
+	marginbot = 10;
+	topareah = hh + ph;
+
+
+	function setpopup(){
+		curpos = $(window).scrollTop(); //current screen scroll from top
+		wh = $(window).height(); //window height
+
+		newheight = 300;
+		//determine top and height
+			if(curpos < topareah ){
+				newtop = topareah + margintop;
+				newheight = wh - newtop + curpos - marginbot;
+				//console.log(newheight);
+			}
+			else{
+				newtop = curpos + margintop;
+				newheight = wh - newtop + curpos - marginbot;
+			}
+			// we are at bottom
+			if(curpos+wh  + 30> fTop){
+				newheight = fTop - curpos - marginbot - margintop - 30;
+			}
+		// set
+		readwrap.css("top",newtop);
+		readwrap.css("height",newheight);
+	}
+
+
+
+
+
+
+	// all of this for wide screen only
+	setpopup();
+	$(window).scroll(function () {
+		setpopup();
+	});
+	$(window).resize(function() {
+		setpopup();
+	});
+
 });
